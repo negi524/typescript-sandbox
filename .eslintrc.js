f7246815e942747c0810897897d7d5fcf1ab02ed
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-10-22 11:34:04 +0900

    install prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    "extends": [
+        "some-other-config-you-use",
+        "prettier"
+    ]
+}
